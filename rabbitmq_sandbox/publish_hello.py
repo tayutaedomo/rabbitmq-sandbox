d1e706a6fadfcc231f9b92c15c2f0744402dea45
tayutaedomo <tayutaedomo@gmail.com>
2023-11-14 00:24:15 +0900

    Update helloworld scripts

--- rabbitmq_sandbox/publish_hello.py
+++ rabbitmq_sandbox/publish_hello.py
@@ -3,11 +3,12 @@ import pika
 
 RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST', 'localhost')
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST))
-channel = connection.channel()
+if __name__ == "__main__":
+    connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST))
+    channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
 
-connection.close()
+    connection.close()
